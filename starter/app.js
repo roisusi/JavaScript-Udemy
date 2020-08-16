8d8e866de5f8d3201e9af507e767b3cb1daf0d68
roisusi <65774200+roisusi@users.noreply.github.com>
2020-08-16 17:57:14 +0300

    Halfway PigGame

--- starter/app.js
+++ starter/app.js
@@ -8,3 +8,77 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+
+
+var scores , roundScore, activePlayer;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+
+
+
+
+//innerHTML make is as HTML code
+//document.querySelector('#current-' + activePlayer).innerHTML = '<h1>' + dice + '</h1>';
+
+//read from element
+//var x = document.querySelector('#score-0').textContent;
+
+//change CSS to hide it
+document.querySelector('.dice').style.display = 'none';
+document.querySelector('.btn-roll').addEventListener('click',btn);
+
+//element by id - make all values to Zero
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+function btn(){
+    //1. Random number
+    var dice = Math.floor(Math.random() * 6 ) +1 ;
+    
+    //2. Display the result
+    //store the selection
+    var diceDOM = document.querySelector('.dice');
+    // block = disply
+    diceDOM.style.display = 'block';
+    //The class id
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. Update the round score and if not rolled 1
+    // !== not useing type currention
+    if (dice !== 1 ){
+        //Add score
+        roundScore += dice;
+        //select elements from a webpage
+        //select things as well as css the first he finds
+        //solution for that - ?
+        //textContent - change the text;
+        document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+    }
+    else{
+        //Next Player
+        //tornery operation
+        activePlayer === 0 ? activePlayer = 1 : activePlayer =0;
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        //change the classes id
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+        //change using toggle
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+    }
+    
+}
+
+//callback funtion that calls another function - btm
+//or write function () { } and write a function instad of btn;
+
